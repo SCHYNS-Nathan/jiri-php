cc5827bd90a07992a640d7269fdea1b529234a09
nathanschyns <nathan.schyns@djmdigital.be>
2024-02-05 16:13:49 +0100

    Changed index to an actual index

--- index.php
+++ index.php
@@ -1,4 +1,72 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-$hello = 'Koukou, comment que ça va ?';
-echo $hello;
+        <title>Jiri</title>
+    </head>
+    <body class="font-sans antialiased bg-light dark:bg-dark selection:bg-black selection:text-white dark:selection:text-black dark:selection:bg-white">
+        <nav>
+            <h2>
+                Menu principal
+            </h2>
+            <ul>
+                <li>
+                    <a href="/jiris">
+                        Jiris
+                    </a>
+                </li>
+                <li>
+                    <a href="/contacts">
+                        Contacts
+                    </a>
+                </li>
+                <li>
+                    <a href="/projects">
+                        Projects
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <main>
+            <h1>
+                Jiris
+            </h1>
+            <section>
+                <h2>
+                    Jiris à venir
+                </h2>
+                <ol>
+                    <li>
+                        <a href="/jiris/1">
+                            Projet web 2025
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/jiris/2">
+                            Design Web 2024
+                        </a>
+                    </li>
+                </ol>
+            </section>
+            <section>
+                <h2>
+                    Jiris terminés
+                </h2>
+                <ol>
+                    <li>
+                        <a href="/jiris/3">
+                            Projet web 2024
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/jiris/4">
+                            Design Web 2023
+                        </a>
+                    </li>
+                </ol>
+            </section>
+        </main>
+    </body>
+</html>
