c4bdd2c9e7c17765b4bd658742890bad5252cd8e
nathanschyns <nathan.schyns@djmdigital.be>
2024-02-05 16:33:48 +0100

    Added tailwind and some css classes

--- index.php
+++ index.php
@@ -5,68 +5,72 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Jiri</title>
+
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
-    <body class="font-sans antialiased bg-light dark:bg-dark selection:bg-black selection:text-white dark:selection:text-black dark:selection:bg-white">
-        <nav>
-            <h2>
-                Menu principal
-            </h2>
-            <ul>
-                <li>
-                    <a href="/jiris">
-                        Jiris
-                    </a>
-                </li>
-                <li>
-                    <a href="/contacts">
-                        Contacts
-                    </a>
-                </li>
-                <li>
-                    <a href="/projects">
-                        Projects
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        <main>
-            <h1>
-                Jiris
-            </h1>
-            <section>
-                <h2>
-                    Jiris à venir
+    <body>
+        <div class="flex flex-col-reverse gap-5 container mx-auto">
+            <main class="flex flex-col gap-5">
+                <h1 class="text-xl font-bold">
+                    Jiris
+                </h1>
+                <section>
+                    <h2>
+                        Jiris à venir
+                    </h2>
+                    <ol>
+                        <li>
+                            <a href="/jiris/1" class="text-blue-500 underline">
+                                Projet web 2025
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/jiris/2" class="text-blue-500 underline">
+                                Design Web 2024
+                            </a>
+                        </li>
+                    </ol>
+                </section>
+                <section>
+                    <h2>
+                        Jiris terminés
+                    </h2>
+                    <ol>
+                        <li>
+                            <a href="/jiris/3" class="text-blue-500 underline">
+                                Projet web 2024
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/jiris/4" class="text-blue-500 underline">
+                                Design Web 2023
+                            </a>
+                        </li>
+                    </ol>
+                </section>
+            </main>
+            <nav class="bg-gray-300">
+                <h2 class="sr-only">
+                    Menu principal
                 </h2>
-                <ol>
+                <ul class="flex gap-4">
                     <li>
-                        <a href="/jiris/1">
-                            Projet web 2025
+                        <a href="/jiris">
+                            Jiris
                         </a>
                     </li>
                     <li>
-                        <a href="/jiris/2">
-                            Design Web 2024
-                        </a>
-                    </li>
-                </ol>
-            </section>
-            <section>
-                <h2>
-                    Jiris terminés
-                </h2>
-                <ol>
-                    <li>
-                        <a href="/jiris/3">
-                            Projet web 2024
+                        <a href="/contacts">
+                            Contacts
                         </a>
                     </li>
                     <li>
-                        <a href="/jiris/4">
-                            Design Web 2023
+                        <a href="/projects">
+                            Projects
                         </a>
                     </li>
-                </ol>
-            </section>
-        </main>
+                </ul>
+            </nav>
+        </div>
     </body>
 </html>
