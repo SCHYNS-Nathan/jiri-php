3d5cfab7d39502ae465e3ee3503d343c5537f1d8
nathanschyns <nathan.schyns@djmdigital.be>
2024-03-24 15:12:21 +0100

    Up to date with screencast

---
+++ core/helpers/functions.php
@@ -0,0 +1,45 @@
+<?php
+
+use JetBrains\PhpStorm\NoReturn;
+
+#[NoReturn] function dd(mixed ...$vars): void
+{
+    foreach ($vars as $var) {
+        var_dump($var);
+        echo '<hr>';
+    }
+    die();
+}
+
+
+function view(string $path, array $data = []): void
+{
+    extract($data);
+
+    $fragments = explode('.', $path);
+
+    require base_path("resources/views/{$fragments[0]}/{$fragments[1]}.view.php");
+}
+
+function base_path(string $path = ''): string
+{
+    return BASE_PATH."/{$path}";
+}
+
+function method(string $method):void
+{
+    echo <<<HTML
+<input type="hidden" name="_method" value="$method">
+
+HTML;
+}
+
+function csrf_token()
+{
+$token = bin2hex(random_bytes(32));
+$_SESSION['csrf_token'] = $token;
+    echo <<<HTML
+<input type="hidden" name="_csrf" value="$token">
+
+HTML;
+}
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\JiriController;
+
+/** @var Core\Router $router */
+$router->get('/', [JiriController::class, 'index']);//done
+
+$router->get('/jiris', [JiriController::class, 'index']);//done
+
+$router->get('/jiri', [JiriController::class, 'show']);//done
+
+$router->get('/jiri/create', [JiriController::class, 'create']);//done
+$router->post('/jiri', [JiriController::class, 'store'])->csrf();//done
+
+$router->get('/jiri/edit', [JiriController::class, 'edit']);
+$router->patch('/jiri', [JiriController::class, 'update'])->csrf();
+
+
+$router->delete('/jiri', [JiriController::class, 'destroy'])->csrf();//done
+
+
